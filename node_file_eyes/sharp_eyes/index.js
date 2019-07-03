835e258a2b97e2d1ddd4a582d19fee3eba243dbd
Falways <1914783590@qq.com>
2019-07-03 13:41:36 +0800

    up

--- node_file_eyes/sharp_eyes/index.js
+++ node_file_eyes/sharp_eyes/index.js
@@ -84,13 +84,18 @@ let asyncTask = (filePath,callback) => {
  * @returns {Promise<void>}
  */
 let readyStarting = async ()=> {
-    let taskArr_ready = {};
+    /*let taskArr_ready = {};
     taskArr_ready['index']=0;
     let path_dir = path.resolve(__dirname,config.watchDirectory)
     let taskJsonArr = fs.readdirSync(path_dir);
     if (taskJsonArr.length>0){
+        logger.info('Deal unfinished task before start watcher...');
         for (let i = 0; i < taskJsonArr.length; i++) {
-            logger.info(`${path_dir}/${taskJsonArr[i]}`);
+            let serialNumber = index + 1;
+            index = index+1;
+            taskArr[serialNumber] = {fp:`${path_dir}/${taskJsonArr[i]}`};
+            eventPools.push(serialNumber);
+            /!*logger.info(`${path_dir}/${taskJsonArr[i]}`);
             taskArr_ready.index = taskArr_ready.index++;
             let serialNumber = taskArr_ready.index+1;
             taskArr[serialNumber] = JSON.parse(fs.readFileSync(`${path_dir}/${taskJsonArr[i]}`));
@@ -107,13 +112,18 @@ let readyStarting = async ()=> {
                     startWatcher();
                     beatBounds();
                 }
-            });
+            });*!/
         }
+        console.log(eventPools)
+        startWatcher();
+        beatBounds();
     }else {
         logger.info(' Complete all before tasks, now start watcher...');
         startWatcher();
         beatBounds();
-    }
+    }*/
+    startWatcher();
+    beatBounds();
 }
 
 // entrance
