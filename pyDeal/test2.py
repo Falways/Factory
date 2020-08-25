e9802aeca1c91aa16af8621a56b7542daebd0114
Falways <live_stronger@163.com>
2020-08-25 17:56:58 +0800

    u

---
+++ pyDeal/test2.py
@@ -0,0 +1,30 @@
+# coding: utf8
+# import binascii
+# python 3.7
+from Crypto.Cipher import AES
+
+class testClass:
+    def __init__(self, key, iv):
+        self.iv = iv
+        self.key = key
+        self.mode = AES.MODE_CBC
+
+        # 加密函数，如果text不是16的倍数【加密文本text必须为16的倍数！】，那就补足为16的倍数
+
+    def encrypt(self, text):
+        cipher = AES.new(self.key, self.mode, self.iv)
+        encodeStr = cipher.encrypt(text)
+        print(encodeStr)
+        print(type(encodeStr))
+        return encodeStr.hex()
+
+
+if __name__ == '__main__':
+    print("test aes-128-cbc")
+    padding = "000000"
+    spacePadding = '\0\0\0\0\0\0\0\0\0\0\0'
+    tc = testClass("Secret=*88"+padding, padding+"999&(Honey")
+    print(testClass.encrypt(tc, spacePadding+'test1'))
+
+# 471ef6f5698f0366af4f73dcdb5fdf06
+# 6fe54ec8fd3899ad7a963a5a91a38bc2
---
+++ pyDeal/test3.py
@@ -0,0 +1,44 @@
+import base64
+from Crypto.Cipher import AES
+
+class AESCipher:
+
+    def __init__(self, key,iv):
+        self.key = key[0:16] #只截取16位
+        self.iv = iv or "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0" # \0 16位字符，用来填充缺失内容，可固定值也可随机字符串，具体选择看需求。
+
+    def __pad(self, text):
+        """填充方式，加密内容必须为16字节的倍数，若不足则使用self.iv进行填充"""
+        text_length = len(text)
+        amount_to_pad = AES.block_size - (text_length % AES.block_size)
+        if amount_to_pad == 0:
+            amount_to_pad = AES.block_size
+        pad = chr(amount_to_pad)
+        return text + pad * amount_to_pad
+
+    def __unpad(self, text):
+        pad = ord(text[-1])
+        return text[:-pad]
+
+    def encrypt(self, raw):
+        """加密"""
+        raw = self.__pad(raw)
+        print('current iv: '+self.iv)
+        cipher = AES.new(self.key, AES.MODE_CBC, self.iv)
+        data = cipher.encrypt(raw)
+        return data.hex()
+
+    def decrypt(self, enc):
+        """解密"""
+        enc = base64.b64decode(enc)
+        cipher = AES.new(self.key, AES.MODE_CBC, self.iv )
+        return self.__unpad(cipher.decrypt(enc).decode("utf-8"))
+
+
+if __name__ == '__main__':
+    e = AESCipher('Secret=*88000000', '000000999&(Honey')
+    secret_data = '"test1"'
+    enc_str = e.encrypt(secret_data)
+    print('enc_str: ' + enc_str)
+    #dec_str = e.decrypt(enc_str)
+    #print('dec str: ' + dec_str)
